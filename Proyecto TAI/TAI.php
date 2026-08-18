1d981b91d46d572fed10d29bb01a445b477cc863
Vicky-Romero <anamarci51@gmail.com>
2026-08-17 21:23:22 -0300

    Add files via upload

--- Proyecto TAI/TAI.php
+++ Proyecto TAI/TAI.php
@@ -8,33 +8,7 @@ include "conexion.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>T.A.I</title>
     <link rel="icon" type="image/png" href="img/logo.png">
-
-    <style>
-    .h2 {
-    text-align: top center;
-    margin-bottom: 25px;
-    }
-
-    .img {
-    text-align: top left;
-    margin-bottom: 25px;
-    }
-
-    .main-menu {
-    text-align: center left;
-    margin-bottom: 25px;
-    }
-
-    .lateral {
-    text-align: center right;
-    margin-bottom: 25px;
-    }
-
-    .boton2 {
-    text-align: center center;
-    margin-bottom: 25px;
-    }
-    </style>
+    <link rel="stylesheet" href="diseño.css">
 </head>
 <body>
     
@@ -42,8 +16,8 @@ include "conexion.php";
 
         <img src="img/logo.png" alt="Logo de la pagina" width="100">
 
-        <h2><title>T.A.I</title></h2>
-        <tittle>Tu Armazon Ideal</tittle>
+        <h2>T.A.I</h2>
+        <p class="subtitulo">Tu Armazón Ideal</p>
         <a href="">Conocenos</a></li>
 
     </header>
@@ -53,8 +27,8 @@ include "conexion.php";
             <li class="menu-item">
                 <span class="menu-btn">Menú ☰</span>
                 <ul class="submenu">
-                    <li><a href="">Lentes</a></li>
-                    <li><a href="">Colorimetria</a></li>
+                    <li><a href="">Armazones</a></li>
+                    <li><a href="">Guía</a></li>
                     <li><a href="">Consultas y Dudas</a></li>
                 </ul>
             </li>
